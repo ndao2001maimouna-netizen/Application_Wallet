806a80dfb2f344f5a80f8d5a38226e8e7f902d2e
maimouna <ndao2001maimouma@gmail.com>
2026-06-28 11:58:15 +0000

    Ajout des validations de saisie

--- services.php
+++ services.php
@@ -20,40 +20,8 @@ function sassieWallet(){
     return $wallet;
 }
 
-//Controle de saissie du creation
-$nom = readline("Nom : ");
+/
 
-if (nomValide($nom) == 0) {
-    echo "Le nom est obligatoire\n";
-    return;
-}
-
-$telephone = readline("Téléphone : ");
-
-if (telephoneValide($telephone) == 0) {
-    echo "Téléphone obligatoire\n";
-    return;
-}
-
-if (telephoneNumerique($telephone) == 0) {
-    echo "Le téléphone doit contenir uniquement des chiffres\n";
-    return;
-}
-
-if (longueurTelephone($telephone) == 0) {
-    echo "Le téléphone doit contenir 9 chiffres\n";
-    return;
-}
-
-if (prefixeValide($telephone) == 0) {
-    echo "Préfixe invalide\n";
-    return;
-}
-
-if (telephoneExiste($telephone, $wallets) == 1) {
-    echo "Ce numéro existe déjà\n";
-    return;
-}
 
 
 
